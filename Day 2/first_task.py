51c6b72dd16bb079187ae70f79c10b1ed1e53bc5
cvirlik <aglevickaa@gmail.com>
2023-12-02 07:07:25 +0100

    Complete Day 2.

---
+++ Day 2/first_task.py
@@ -0,0 +1,31 @@
+import re
+
+red_count = 12
+green_count = 13
+blue_count = 14
+
+def is_possible(game_line: str):
+    game_id, game_record = game_line.split(": ")
+    game_id = int(re.findall(r'\d+', game_id)[0])
+    game_record = game_record.split("; ")
+    for record in game_record:
+        colors = record.split(", ")
+        for color in colors:
+            num, name = color.split(" ")
+            if name == "red" and int(num) > red_count:
+                return (False, game_id)
+            elif name == "green" and int(num) > green_count:
+                return (False, game_id)
+            elif name == "blue" and int(num) > blue_count:
+                return (False, game_id)
+    return (True, game_id)
+
+
+if __name__ == '__main__':
+    sum = 0
+    with open("Day 2/input.txt") as file:
+        for line in file.read().split('\n'):
+            b, n = is_possible(line)
+            if b:
+                sum += n
+    print(sum)
---
+++ Day 2/second_task.py
@@ -0,0 +1,30 @@
+import re
+
+
+def is_possible(game_line: str):
+    game_id, game_record = game_line.split(": ")
+    game_id = int(re.findall(r'\d+', game_id)[0])
+    game_record = game_record.split("; ")
+    r, g, b = -1, -1, -1
+    for record in game_record:
+        colors = record.split(", ")
+        for color in colors:
+            num, name = color.split(" ")
+            num = int(num)
+            if name == "red":
+                r = max(num, r)
+            elif name == "green":
+                g = max(num, g)
+            elif name == "blue":
+                b = max(num, b)
+    return (r, g, b)
+
+
+if __name__ == '__main__':
+    sum = 0
+    with open("Day 2/input.txt") as file:
+        for line in file.read().split('\n'):
+            r, g, b = is_possible(line)
+            if b:
+                sum += r*g*b
+    print(sum)
