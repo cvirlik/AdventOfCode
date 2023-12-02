5a58dfe128a6d1f91bc7676ddbb1b2b521781c22
cvirlik <aglevickaa@gmail.com>
2023-12-02 03:31:05 +0100

    Complete Day 1.

---
+++ Day 1/first_task.py
@@ -0,0 +1,14 @@
+import re
+
+
+def get_num(text_line: str):
+    return (int(re.search(r'\d+', text_line).group()[0]) * 10 +
+            int(re.search(r'\d+', text_line[::-1]).group()[0]))
+
+
+if __name__ == '__main__':
+    sum = 0
+    with open('input.txt') as file:
+        for line in file:
+            sum += get_num(line)
+    print(sum)
---
+++ Day 1/second_task.py
@@ -0,0 +1,35 @@
+import regex as re
+
+num_str = {
+    "one": 1,
+    "two": 2,
+    "three": 3,
+    "four": 4,
+    "five": 5,
+    "six": 6,
+    "seven": 7,
+    "eight": 8,
+    "nine": 9
+}
+
+
+def convert_to_num(str_num: str, mul: int):
+    if str_num.isdigit():
+        return int(str_num) * mul
+    else:
+        return num_str[str_num] * mul
+
+
+def get_num(text_line: str):
+    numbers_pattern = r'\d|one|two|three|four|five|six|seven|eight|nine'
+
+    regex_result = re.findall(numbers_pattern, text_line, overlapped=True)
+    return convert_to_num(regex_result[0], 10) + convert_to_num(regex_result[-1], 1)
+
+
+if __name__ == '__main__':
+    sum = 0
+    with open('input.txt') as file:
+        for line in file:
+            sum += get_num(line)
+    print(sum)
